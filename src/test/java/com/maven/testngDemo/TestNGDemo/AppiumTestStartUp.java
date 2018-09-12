b966664fbde52565d6e8ab8484bea5cb6e38985a
mathewS <mathewS@wddcewind12.mkcorp.com>
2018-09-12 15:30:47 -0500

    Demo 3

--- src/test/java/com/maven/testngDemo/TestNGDemo/AppiumTestStartUp.java
+++ src/test/java/com/maven/testngDemo/TestNGDemo/AppiumTestStartUp.java
@@ -33,7 +33,7 @@ public class AppiumTestStartUp {
 	public static void testSetUp() {
 		System.out.println("Starting Appium server" + df.format(new Date()));
 		startAppiumServer();
-		//Commented2
+		//Commented23
 	}
 
 	@AfterTest
